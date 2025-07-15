b660727cfd4334ad86f54290e0a0b979edb4896f
911374 <911374@LTIN452093.cts.com>
2025-07-15 11:37:15 +0530

    new

---
+++ src/main/java/action/mouseaction.java
@@ -0,0 +1,27 @@
+package action;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class mouseaction {
+	static WebDriver driver;
+	public static void main(String[] args) throws Exception {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demo.opencart.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		WebElement desktop = driver.findElement(By.xpath("//a[normalize-space()='Desktops']"));
+		WebElement mac = driver.findElement(By.xpath("//a[normalize-space()='Mac (1)']"));
+        
+		Actions act = new Actions(driver);
+		act.moveToElement(desktop).moveToElement(mac).build().perform();
+		
+		act.contextClick(mac);
+		act.doubleClick(desktop);
+	}
+
+}
