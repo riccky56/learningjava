1218f3a7d2c9c1492a10e54076abc7eeb5b7bfeb
911374 <911374@LTIN452093.cts.com>
2025-07-16 16:50:11 +0530

    y

--- src/main/java/action/windowhandling.java
+++ src/main/java/action/windowhandling.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class windowhandling {
-  WebDriver driver;
+     WebDriver driver;
 	public static void main(String[] args) {
 			WebDriver driver = new ChromeDriver();
 
