ce53535502f65e147a31149797901584e141a1b2
911374 <911374@LTIN452093.cts.com>
2025-07-17 11:20:31 +0530

    new

---
+++ src/main/java/action/javascriptdemo.java
@@ -0,0 +1,33 @@
+package action;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class javascriptdemo {
+	
+  static WebDriver driver;
+	public static void main(String[] args) {
+
+		driver = new ChromeDriver();
+
+		driver.get("https://demo.nopcommerce.com/");
+
+		driver.manage().window().maximize();
+
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+
+		js.executeScript("document.body.style.zoom='50%'");
+
+		js.executeScript("document.body.style.zoom='80%'");
+
+		js.executeScript("document.body.style.zoom='70%'");
+
+		js.executeScript("document.body.style.zoom='90%'");
+
+
+
+
+	}
+
+}
