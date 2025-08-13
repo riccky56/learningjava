b6eb76497a7d3b9440861787974c38cef0a804f0
Rahul Gupta <rahulgupta.12328@gmail.com>
2025-08-14 01:11:21 +0530

    m

--- src/test/java/SeleniumTutorial/Learning/flipkart.java
+++ src/test/java/SeleniumTutorial/Learning/flipkart.java
@@ -32,7 +32,7 @@ public class flipkart extends flipkartbrowser{
 		driver.findElement(By.xpath("//div[@class='I-qZ4M vLRlQb']/input")).sendKeys("9131042507");
 		driver.findElement(By.xpath("(//*[normalize-space()='Request OTP'])[1]")).click();
 		
-		Thread.sleep(9000);
+		Thread.sleep(10000);
 		WebElement search = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']")); 
 		Thread.sleep(2000);
 		search.sendKeys("Samsung s22");
@@ -82,7 +82,7 @@ public class flipkart extends flipkartbrowser{
 		driver.findElement(By.xpath("//button[@class='QqFHMw zA2EfJ _7Pd1Fp']")).click();
 		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("(//*[@type='radio'])[3]")).click();
+		driver.findElement(By.xpath("(//*[@type='radio'])[2]")).click();
 		
 		
 		
