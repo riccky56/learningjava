0400a73a8dc5da2d6eb55fcf463b3a4d6e71ecd6
Rahul Gupta <rahulgupta.12328@gmail.com>
2025-08-26 13:36:38 +0530

    SVG UPDATE

---
+++ src/main/java/action/Svgelement.java
@@ -0,0 +1,31 @@
+package action;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Svgelement {
+	static WebDriver driver;
+
+	@Test 
+	public void method1()
+	{
+
+		driver= new ChromeDriver();
+
+		driver.get("https://www.tutorialspoint.com/selenium/practice/login.php");
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.findElement(By.xpath("//a[normalize-space()='Practice Form']//*[name()='svg']")).click();
+
+
+	}
+
+}
