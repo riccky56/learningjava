288e94de34bb9428d8f253e5a731426a17899021
Rahul Gupta <rahulgupta.12328@gmail.com>
2026-06-30 14:32:13 +0530

    updating changes

--- src/test/java/JavaBasics/arrays.java
+++ src/test/java/JavaBasics/arrays.java
@@ -14,8 +14,6 @@ public class arrays {
 
 		boolean myCard = true;
 
-
-
 		System.out.println(myNum+"is the value stored in the myNum variable");
 
 		System.out.println(website);
@@ -28,7 +26,8 @@ public class arrays {
 
 		arr[3]= 5; 		arr[4]= 6;
 
-
+//best way to initialize and assign value to a array  
+		
 		int[] arr2 = {1,2,4,5,6};
 
 		System.out.println(arr2[0]);
@@ -42,13 +41,13 @@ public class arrays {
 		for(int i = 0; i< arr.length; i++) //4
 
 		{
-			System.out.print(arr[i]);
+			System.out.println(arr[i]);
 		}
 
 		for (int i =0;i<arr2.length; i++)
 
 		{
-			System.out.println(arr2[i]);
+			System.out.print(arr2[i]);
 		}
 
 		String[] name = {"rahul", "shetty", "selenium"};
@@ -62,17 +61,8 @@ public class arrays {
 		for( String s: name)
 
 		{
-
 			System.out.println(s);
-
 		}
 
-
-
 	}
-
-
-
-
-
 }
