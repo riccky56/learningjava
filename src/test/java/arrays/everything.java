1b3bfb88cb6b70b30d28bd5c39a90803f92f0675
Rahul Gupta <rahulgupta.12328@gmail.com>
2026-09-20 00:22:36 +0530

    java codes

--- src/test/java/arrays/everything.java
+++ src/test/java/arrays/everything.java
@@ -5,38 +5,38 @@ public class everything {
 
 	// Main method where program execution starts.
 	public static void main(String[] args) {
-		
+
 		// Store text data that will be processed by the program logic.
 		String a = "Test1233Gtsag!@#$dwdb1234";
-		
+
 		// Store text data that will be processed by the program logic.
-		String letters ="";
+		String letters = "";
 		// Store text data that will be processed by the program logic.
-		String digit ="";
+		String digit = "";
 		// Store text data that will be processed by the program logic.
-		String special ="";
+		String special = "";
 		// Loop through each element one by one.
-		for( char c :a.toCharArray()) {
-			
+		for (char c : a.toCharArray()) {
+
 			// Check the condition before deciding whether this block should run.
-			if(Character.isLetter(c)) {
+			if (Character.isLetter(c)) {
 				// Update the variable based on the current calculation.
 				letters = letters + c;
-			}else if(Character.isDigit(c)) {
+			} else if (Character.isDigit(c)) {
 				// Update the variable based on the current calculation.
 				digit = digit + c;
-			}else {
+			} else {
 				// Update the variable based on the current calculation.
-				special = special+c;
+				special = special + c;
 			}
-			
+
 		}
 		// Display information to the console for the user.
-		System.out.println("Printed Letters : " +letters);
+		System.out.println("Printed Letters : " + letters);
 		// Display information to the console for the user.
-		System.out.println("Printed Letters : " +digit);
+		System.out.println("Printed Letters : " + digit);
 		// Display information to the console for the user.
-		System.out.println("Printed specials : " +special);
+		System.out.println("Printed specials : " + special);
 
 	}
 
